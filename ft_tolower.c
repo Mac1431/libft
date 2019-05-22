4d0dc8189b23c080c9b82df2ac0df5d08fa46474
Phetho MALOPE <pmalope@c4r3s1.wethinkcode.co.za>
2019-05-22 15:01:26 +0200

    fixed norm errors

--- ft_tolower.c
+++ ft_tolower.c
@@ -6,13 +6,16 @@
 /*   By: pmalope <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 08:34:38 by pmalope           #+#    #+#             */
-/*   Updated: 2019/05/21 14:34:29 by pmalope          ###   ########.fr       */
+/*   Updated: 2019/05/22 08:55:25 by pmalope          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int     ft_tolower(int c)
 {
-    if (c >= 'A' && c <= 'Z')
-        c += 32;
-    return (c);
+    unsigned char res;
+
+    res = (unsigned char)c;
+    if (res >= 'A' && res <= 'Z')
+        res += 32;
+    return (res);
 }
