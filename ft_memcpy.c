40df1fc0383cf28c151ec082621c8e73f41846af
Phetho MALOPE <pmalope@c5r3s9.wethinkcode.co.za>
2019-05-25 01:33:12 +0200

    adding ft_memcpy.c file

---
+++ ft_memcpy.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmalope <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/25 01:09:42 by pmalope           #+#    #+#             */
+/*   Updated: 2019/05/25 01:17:16 by pmalope          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	char		*ptr_dst;
+	const char	*ptr_src;
+
+	ptr_dst = dst;
+	ptr_src = src;
+	while (n > 0)
+	{
+		*ptr_dst = *ptr_src;
+		ptr_dst++;
+		ptr_src++;
+		n--;
+	}
+	return (dst);
+}
