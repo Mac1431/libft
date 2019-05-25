23c60ac1bf794f0066b583ccd4e699be9d086194
Phetho MALOPE <pmalope@c4r3s1.wethinkcode.co.za>
2019-05-25 12:06:28 +0200

    changed logic

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: pmalope <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/25 08:48:37 by pmalope           #+#    #+#             */
-/*   Updated: 2019/05/25 11:53:47 by pmalope          ###   ########.fr       */
+/*   Updated: 2019/05/25 12:03:09 by pmalope          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void    *ft_memccpy(void *dst, const void *src, int c, size_t n)
 
     ptr_d = dst;
     ptr_s = src;
-    while (n > 0 || *ptr_s == (unsigned char )c)
+    while (n > 0 && *ptr_s != (unsigned char )c)
     {
         *ptr_d = *ptr_s;
         ptr_d++;
