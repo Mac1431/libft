a1626608c2a8cc95d0fa1bc50cf0074ee1ce560f
Phetho MALOPE <pmalope@c4r3s1.wethinkcode.co.za>
2019-05-25 11:10:36 +0200

    adding ft_memmove.c file

---
+++ ft_memmove.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmalope <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/25 08:50:16 by pmalope           #+#    #+#             */
+/*   Updated: 2019/05/25 09:31:24 by pmalope          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void    *ft_memmove(void *dst, const void *src, size_t len)
+{
+    unsigned char temp[len];
+
+    ft_memcpy(temp, src, len);
+    ft_memcpy(dst, temp, len);
+    return (dst);
+}
