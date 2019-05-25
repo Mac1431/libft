966419a1079fca615ac5e53588e722596adf0afd
Phetho MALOPE <pmalope@c4r3s1.wethinkcode.co.za>
2019-05-25 11:27:01 +0200

    added function prototypes

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: pmalope <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 13:47:26 by pmalope           #+#    #+#             */
-/*   Updated: 2019/05/25 01:21:05 by pmalope          ###   ########.fr       */
+/*   Updated: 2019/05/25 11:13:42 by pmalope          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,5 +40,8 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
-void	*memccpy(void *dst, const void *src, int c, size_t n);
+void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
+void    *ft_memmove(void *dst, const void *src, size_t len);
+void    *ft_memchr(const void *s, int c, size_t n);
+int     ft_memcmp(const void *s1, const void *s2, size_t n);
 #endif
